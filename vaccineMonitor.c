dbd0967df9b90d53a6dd0750777e4c51ff749a30
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-03-07 15:35:51 +0200

    Read command line arguments

--- vaccineMonitor.c
+++ vaccineMonitor.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "list.h"
 
 int main(int argc, char** argv){
 	
+	/*	---		DECLARATIONS	---	*/
+
+	int i, bloomSize;
+
+
+
+	FILE* citizenRecordsFile;
+
+	/*		---------------		*/
+
+	if(argc < 5){							//	read arguments from command line
+		printf("Wrong arguments!!!\n");
+		return -1;
+	}
+	else{
+		for(i = 0; i < 4; i++){	
+			if(!strcmp(argv[i], "-c")){
+				if(!(citizenRecordsFile = fopen(argv[i+1], "r"))){
+					printf("Error in opening %s\n", argv[i+1]);
+					return -1;
+				}
+			}
+			else if(!strcmp(argv[i], "-b")){
+				bloomSize = atoi(argv[i+1]);
+			}
+		}
+	}
+
+	
+	
+	return 0;
 }

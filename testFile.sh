6d2dfd14ad9bdfcbab9248f207a2f1be262e3446
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-03-14 22:55:22 +0200

    Bash script

--- testFile.sh
+++ testFile.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 
+for(( i=0; i<$3; i++ ))
+do
+	echo -n $((1 + $RANDOM % 9999)); echo -n " "
+	tr -dc A-Za-z </dev/urandom | head -c $((3 + $RANDOM % 10)); echo -n " "
+	tr -dc A-Za-z </dev/urandom | head -c $((3 + $RANDOM % 10)); echo -n " "
+	shuf -n 1 $2| tr -d '\n'; echo -n " "
+	echo -n $((1 + $RANDOM % 120)); echo -n " "
+	shuf -n 1 $1;
+done

595bdca8817b4eeb9db14049b2a353009f7e0e85
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-04-07 01:38:52 +0300

    Bash script duplicates not allowed OK

--- testFile.sh
+++ testFile.sh
@@ -10,6 +10,7 @@ arr[1]="NO"
 declare -a temp_array
 declare -a viruses
 declare -a countries
+declare -a uniqueIDs
 
 if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
 	if [[ $3 > 0 && $4 == 0 || $4 == 1 ]]; then	#check values of numbers given
@@ -45,8 +46,29 @@ if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
 				else
 					printf "\n" >> inputFile;
 				fi
-			else
-				printf "duplicatesAllowed NO\n";
+			else		#duplicates not allowed
+				temp_array[0]=$((1 + $RANDOM % 9999));
+				while [[ " ${uniqueIDs[@]} " =~ " ${temp_array[0]} " ]]; do 	#check if array contains value
+				    temp_array[0]=$((1 + $RANDOM % 9999));
+				done
+				echo -n ${temp_array[0]} >> inputFile; echo -n " " >> inputFile;		#citizenID
+				tr -dc A-Za-z </dev/urandom | head -c $((3 + $RANDOM % 10)) >> inputFile; echo -n " " >> inputFile;		#firstName
+				tr -dc A-Za-z </dev/urandom | head -c $((3 + $RANDOM % 10)) >> inputFile; echo -n " " >> inputFile;		#lastName
+				echo ${countries[$RANDOM % ${#countries[@]} ]}| tr -d '\n' >> inputFile; echo -n " " >> inputFile;		#country
+				echo -n $((1 + $RANDOM % 120)) >> inputFile; echo -n " " >> inputFile;		#age
+				echo ${viruses[$RANDOM % ${#viruses[@]} ]}| tr -d '\n' >> inputFile; echo -n " " >> inputFile;		#virusName
+				rand=$[ $RANDOM % 2 ];echo ${arr[$rand]}| tr -d '\n' >> inputFile;	#YES/NO
+				if [[ $rand == 0 ]]; then				#if YES
+					rand=$[ $RANDOM % 2 ];
+					case $rand in
+						"0")
+							echo -n " " >> inputFile; echo -n $((1 + $RANDOM % 30)) >> inputFile; echo -n "-" >> inputFile; echo -n $((1 + $RANDOM % 12)) >> inputFile; echo -n "-" >> inputFile;echo $((1900 + $RANDOM % 120)) >> inputFile;;
+						"1")
+							printf "\n" >> inputFile;
+					esac
+				else
+					printf "\n" >> inputFile;
+				fi
 			fi
 		done
 	else

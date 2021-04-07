b3057be95e43925ab443d4b01dbfbe96571dfa16
Savvas-Pol <sdi1200150@di.uoa.gr>
2021-04-07 03:35:00 +0300

    Bash script - multiple duplicate records need to fix

--- testFile.sh
+++ testFile.sh
@@ -26,7 +26,7 @@ if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
 		for(( i=0; i<$3; i++ ))
 		do
 			if [[ $4 == 1 ]]; then	#if duplicatesAllowed
-				#rand=$[ $RANDOM % 2 ];
+				temp_rand=$(($RANDOM % 4));
 				temp_array[0]=$((1 + $RANDOM % 9999));
 				echo -n ${temp_array[0]} >> inputFile; echo -n " " >> inputFile;		#citizenID
 				tr -dc A-Za-z </dev/urandom | head -c $((3 + $RANDOM % 10)) >> inputFile; echo -n " " >> inputFile;		#firstName
@@ -46,6 +46,28 @@ if [[ $ARGC == $MAX_ARGS ]]; then	#check command line arguments
 				else
 					printf "\n" >> inputFile;
 				fi
+				: '
+				for(( j=0; j<$temp_rand; j++ ))		#print duplicate records in some cases
+				do
+					echo -n ${temp_array[0]} >> inputFile; echo -n " " >> inputFile;		#citizenID
+					echo -n ${temp_array[1]} >> inputFile; echo -n " " >> inputFile;		#firstName
+					echo -n ${temp_array[2]} >> inputFile; echo -n " " >> inputFile;		#lastName
+					echo -n ${temp_array[3]} >> inputFile; echo -n " " >> inputFile;		#country
+					echo -n ${temp_array[4]} >> inputFile; echo -n " " >> inputFile;		#age
+					echo ${viruses[$RANDOM % ${#viruses[@]} ]}| tr -d '\n' >> inputFile; echo -n " " >> inputFile;		#virusName
+					rand=$[ $RANDOM % 2 ];echo ${arr[$rand]}| tr -d '\n' >> inputFile;	#YES/NO
+					if [[ $rand == 0 ]]; then				#if YES
+						rand=$[ $RANDOM % 2 ];
+						case $rand in
+							"0")
+								echo -n " " >> inputFile; echo -n $((1 + $RANDOM % 30)) >> inputFile; echo -n "-" >> inputFile; echo -n $((1 + $RANDOM % 12)) >> inputFile; echo -n "-" >> inputFile;echo $((1900 + $RANDOM % 120)) >> inputFile;;
+							"1")
+								printf "\n" >> inputFile;
+						esac
+					else
+						printf "\n" >> inputFile;
+					fi
+				done'
 			else		#duplicates not allowed
 				temp_array[0]=$((1 + $RANDOM % 9999));
 				while [[ " ${uniqueIDs[@]} " =~ " ${temp_array[0]} " ]]; do 	#check if array contains value
